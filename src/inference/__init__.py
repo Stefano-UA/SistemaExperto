4b727e647465ff7846a8eb2a046b2eb4310d3c27
Stefano Bia <sbc66@alu.ua.es>
2026-05-13 13:13:38 +0200

    Added inference package as a whole

---
+++ src/inference/__init__.py
@@ -0,0 +1,7 @@
+'''
+Inference Subsystem
+===================
+This package contains the core intelligence of the application. It provides the base
+infrastructure and specialized engines capable of executing evaluations, whether
+through deterministic rules (MBI) or advanced logical operators over fuzzy sets.
+'''
